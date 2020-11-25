36baafa57c47a22353b7a768277218ad2996e759
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-11-25 10:30:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+if (time.split(':')[0].parseInt()<12)
+console.log("Good Morning");
+else if ((time.split(':')[0].parseInt()>12)&&(time.split(':')[0].parseInt()<5))
+console.log("Good Afternoon");
+else if (time.split(':')[0].parseInt()>5)
+console.log("Good Evening";)
+}
 /* Write your implementation of displayMessage() */
